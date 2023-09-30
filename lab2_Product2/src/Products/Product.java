af4f5e37592e9da1deae3431ea6ddf0bc5577824
Anastasia <125199635+AnastasiaFalk@users.noreply.github.com>
2023-09-30 23:01:14 +0100

    Update Product.java

--- lab2_Product2/src/Products/Product.java
+++ lab2_Product2/src/Products/Product.java
@@ -1,6 +1,7 @@
 package Products;
 
 public class Product {
+    // задання приватних полів класу
     private int id;
     private String name;
     private String manufacturer;
@@ -8,65 +9,57 @@ public class Product {
     private int shelfLife;
     private int quantity;
 
-    public Product(int id, String name, String manufacturer, double price, int shelfLife, int quantity) {
-        this.id = id;
-        this.name = name;
-        this.manufacturer = manufacturer;
-        this.price = price;
-        this.shelfLife = shelfLife;
-        this.quantity = quantity;
+    // конструктор класу
+    public Product(int id, String name, String manufacturer, double price, int shelfLife, int quantity) { // конструктор класу `Product`, який приймає шість параметрів
+        this.id = id; // присвоює переданий параметр `id` полю `id` нового об'єкта
+        this.name = name; // присвоює переданий параметр `name` полю `name` нового об'єкта
+        this.manufacturer = manufacturer; // присвоює переданий параметр `manufacturer` полю `manufacturer` нового об'єкта
+        this.price = price; // присвоює переданий параметр `price` полю `price` нового об'єкта
+        this.shelfLife = shelfLife; // присвоює переданий параметр `shelfLife` полю `shelfLife` нового об'єкта
+        this.quantity = quantity; // присвоює переданий параметр `quantity` полю `quantity` нового об'єкта
     }
 
+    // геттери і сеттери для кожного поля класу
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getManufacturer() {
         return manufacturer;
     }
-
     public void setManufacturer(String manufacturer) {
         this.manufacturer = manufacturer;
     }
-
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
     }
-
     public int getShelfLife() {
         return shelfLife;
     }
-
     public void setShelfLife(int shelfLife) {
         this.shelfLife = shelfLife;
     }
-
     public int getQuantity() {
         return quantity;
     }
-
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
+    // перевизначений метод toString()
     @Override
-    public String toString() {
+    public String toString() { // перевизначає метод `toString()` класу `Object`, повертає текстове представлення об'єкта `Product`
         return
                 "----------------" + '\n' +
                 "- Продукт " + name + ":" + '\n' +
